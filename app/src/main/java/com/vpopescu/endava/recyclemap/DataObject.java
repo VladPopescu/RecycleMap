2d920bc1c62730dfc23d8c2a8914b80060cf65ac
unknown <vlad.popescu@endava.com>
2017-10-08 02:06:01 +0300

    Added ViewPager with Custom Adapter'

---
+++ app/src/main/java/com/vpopescu/endava/recyclemap/DataObject.java
@@ -0,0 +1,33 @@
+package com.vpopescu.endava.recyclemap;
+
+public class DataObject {
+    private int imageId;
+    private String imageName;
+    private String imageText;
+
+    public DataObject(int imageId, String imageName, String imageText) {
+        this.imageId = imageId;
+        this.imageName = imageName;
+        this.imageText = imageText;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getImageName() {
+        return imageName;
+    }
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageText() {
+        return imageText;
+    }
+
+    public void setImageText(String imageText) {
+        this.imageText = imageText;
+    }
+}
--- app/src/main/java/com/vpopescu/endava/recyclemap/MainActivity.java
+++ app/src/main/java/com/vpopescu/endava/recyclemap/MainActivity.java
@@ -1,13 +1,31 @@
 package com.vpopescu.endava.recyclemap;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<DataObject> getData = dataSource();
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        CustomPageAdapter mCustomPagerAdapter = new CustomPageAdapter(MainActivity.this, getData);
+        viewPager.setAdapter(mCustomPagerAdapter);
+    }
+    private List<DataObject> dataSource(){
+        List<DataObject> data = new ArrayList<DataObject>();
+
+        data.add(new DataObject(R.mipmap.find_recycle_center, "Find recycle centers!", "SADa sjajhjhja jashf vashashfas ashsa hsa hhs aheja"));
+        data.add(new DataObject(R.mipmap.collect_waste_from_home, "Collect the waste from my place!","ANVD aASVa AHSFASHJFSA ASHAASA sajhkasfba jaksfbakhsfkafjala"));
+        data.add(new DataObject(R.mipmap.add_waste_center, "Add new recycle centers!", "Desrahkfabd sajfksbh bha sh hasab bhkhhkakhkha sadasgae"));
+
+        return data;
     }
 }
