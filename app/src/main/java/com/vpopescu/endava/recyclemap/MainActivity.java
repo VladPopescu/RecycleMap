7615679add2f2c5979e3254ef9362b8d4a807d9c
unknown <vlad.popescu@endava.com>
2017-10-08 13:49:51 +0300

    Addedd ViewPagerwith transition

--- app/src/main/java/com/vpopescu/endava/recyclemap/MainActivity.java
+++ app/src/main/java/com/vpopescu/endava/recyclemap/MainActivity.java
@@ -4,21 +4,60 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private Button slidePageButton;
+    private int dotsCount=3;
+    private ImageView[] dots;
+    LinearLayout linearLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<DataObject> getData = dataSource();
-        viewPager = (ViewPager)findViewById(R.id.viewpager);
         CustomPageAdapter mCustomPagerAdapter = new CustomPageAdapter(MainActivity.this, getData);
+        slidePageButton = (Button) findViewById(R.id.buttonSlidePage);
+        drawPageSelectionIndicators(0);
+
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
         viewPager.setAdapter(mCustomPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                drawPageSelectionIndicators(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        slidePageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawPageSelectionIndicators(viewPager.getNextFocusRightId());
+            }
+        });
+
     }
+
+
     private List<DataObject> dataSource(){
         List<DataObject> data = new ArrayList<DataObject>();
 
@@ -27,5 +66,28 @@ public class MainActivity extends AppCompatActivity {
         data.add(new DataObject(R.mipmap.add_waste_center, "Add new recycle centers!", "Desrahkfabd sajfksbh bha sh hasab bhkhhkakhkha sadasgae"));
 
         return data;
+    }
+
+    private void drawPageSelectionIndicators(int mPosition){
+        if(linearLayout!=null) {
+            linearLayout.removeAllViews();
+        }
+        linearLayout=(LinearLayout)findViewById(R.id.viewPagerCountDots);
+        dots = new ImageView[dotsCount];
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(getApplicationContext());
+            if(i==mPosition)
+                dots[i].setImageDrawable(getResources().getDrawable(R.drawable.item_selected));
+            else
+                dots[i].setImageDrawable(getResources().getDrawable(R.drawable.item_unselected));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            params.setMargins(4, 0, 4, 0);
+            linearLayout.addView(dots[i], params);
+        }
     }
 }
